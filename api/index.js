76f762c027cd21a2b30edd68997516367986b45f
Vincent Claeys <vince@MacBook-Pro-van-Vincent.local>
2023-05-29 09:24:50 +0200

    some changes

--- api/index.js
+++ api/index.js
@@ -866,7 +866,7 @@ app.use(async (req, res, next) => {
 }, authRouter);
 
 app.listen(port, () => {
-  console.log(`App listening http://localhost:${port}`);
+  console.log(`App listening https://immoapi-production.up.railway.app:${port}`);
 });
 
 // make sure database connection is closed when server crashes
