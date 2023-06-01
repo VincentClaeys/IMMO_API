87479a991ed81452310fa9cb7b855d4715a33da3
Vincent Claeys <vince@MacBook-Pro-van-Vincent.local>
2023-06-01 18:07:54 +0200

    new try cors komop api pls sssss d

--- api/index.js
+++ api/index.js
@@ -872,7 +872,7 @@ app.use(async (req, res, next) => {
 }, authRouter);
 
 app.listen(port,() => {
-  console.log(`App listening https://immoapi-production.up.railway.app:${port}`);
+  console.log(`App listening https://localhost:${port}`);
 });
 
 // make sure database connection is closed when server crashes
